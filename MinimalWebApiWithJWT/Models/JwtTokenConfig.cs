caecea74f4993fdca108373ba512e084b2e642e7
Ibrahim <inobani28@gmail.com>
2023-11-21 22:52:48 +0200

    Adding the required models (User, UserAuth and JwtTokenConfig for the the token configuration

---
+++ MinimalWebApiWithJWT/Models/JwtTokenConfig.cs
@@ -0,0 +1,8 @@
+namespace MinimalWebApi.Models;
+
+public class JwtTokenConfig
+{
+    public string SecretKey { get; set; }
+    public string Issuer { get; set; }
+    public string Audience { get; set; }
+}
